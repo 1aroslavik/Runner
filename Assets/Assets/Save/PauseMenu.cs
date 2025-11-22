31c515fa5d5b071103e5ef4a3551f99af541d502
Iaroslava Babenco <babenkoiaroslava@gmail.com>
2025-11-22 22:43:13 +0200

    pause menu

---
+++ Assets/Assets/Save/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu Instance;
+
+    public GameObject pauseUI; // сам Canvas PauseMenu
+    private bool isPaused = false;
+
+    void Awake()
+    {
+        Instance = this;
+        pauseUI.SetActive(false); // скрыть меню при старте
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void SaveGame()
+    {
+        PermanentStats.Instance.SavePermanentData();
+        Debug.Log("✔ Игра сохранена");
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
